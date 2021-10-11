1a2839a319c517548385d41a74256347b22452a8
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-10-11 10:04:45 -0400

    added query and mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,4 +23,23 @@ export const typeDefs = gql`
     token: ID!
     user: User
   }
+
+  type Query {
+    login: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(
+      authors: [String]
+      Description: String!
+      title: String!
+      bookid: String
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: String!): User
+  }
 `;
