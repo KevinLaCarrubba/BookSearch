4a8ed8906fc05260e64db1bfd55fd5be9468361b
Kevin LaCarrubba <Kevin.LaCarrubba@yahoo.com>
2021-10-11 10:00:48 -0400

    added User typedef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,21 @@
 const { gql } = require("apollp-server-express");
+
+export const typeDefs = gql`
+  type Book {
+    _id: ID
+    bookId: ID!
+    authors: [String]
+    description: String!
+    title: String!
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+`;
